049ef9c15b80f54bd2dfc7e04f73c29bddb36072
Marcin <06mar3in@gmail.com>
2022-07-07 14:11:01 +0200

    Poprawki

--- Fizyka/App.xaml.cs
+++ Fizyka/App.xaml.cs
@@ -12,38 +12,22 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.UI.Xaml;
 
-// To learn more about WinUI3, see: https://docs.microsoft.com/windows/apps/winui/winui3/.
 namespace Fizyka;
 
 public partial class App : Application
 {
-    // The .NET Generic Host provides dependency injection, configuration, logging, and other services.
-    // https://docs.microsoft.com/dotnet/core/extensions/generic-host
-    // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
-    // https://docs.microsoft.com/dotnet/core/extensions/configuration
-    // https://docs.microsoft.com/dotnet/core/extensions/logging
     private static readonly IHost _host = Host
         .CreateDefaultBuilder()
         .ConfigureServices((context, services) =>
         {
-            // Default Activation Handler
             services.AddTransient<ActivationHandler<LaunchActivatedEventArgs>, DefaultActivationHandler>();
-
-            // Other Activation Handlers
-
-            // Services
             services.AddSingleton<IThemeSelectorService, ThemeSelectorService>();
             services.AddSingleton<ILocalSettingsService, LocalSettingsServiceUnpackaged>();
             services.AddTransient<INavigationViewService, NavigationViewService>();
-
             services.AddSingleton<IActivationService, ActivationService>();
             services.AddSingleton<IPageService, PageService>();
             services.AddSingleton<INavigationService, NavigationService>();
-
-            // Core Services
             services.AddSingleton<IFileService, FileService>();
-
-            // Views and ViewModels
             services.AddTransient<RuchJednostajeniePrzyspieszonyViewModel>();
             services.AddTransient<RuchJednostajeniePrzyspieszonyPage>();
             services.AddTransient<RuchPrzyspieszonyViewModel>();
@@ -68,8 +52,6 @@ public partial class App : Application
             services.AddTransient<OpisPage>();
             services.AddTransient<ShellPage>();
             services.AddTransient<ShellViewModel>();
-
-            // Configuration
             services.Configure<LocalSettingsOptions>(context.Configuration.GetSection(nameof(LocalSettingsOptions)));
         })
         .Build();
@@ -89,10 +71,7 @@ public partial class App : Application
     }
 
     private void App_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-    {
-        // TODO: Log and handle exceptions as appropriate.
-        // For more details, see https://docs.microsoft.com/windows/winui/api/microsoft.ui.xaml.unhandledexceptioneventargs.
-    }
+    {}
 
     protected async override void OnLaunched(LaunchActivatedEventArgs args)
     {
